f699be9fa6f4ffb6134300bc945349de18fd0dad
Edwuards <cesar.edwuards@gmail.com>
2022-11-09 10:55:57 -0600

    wip  :

--- src/gráficos/cuadrado.ts
+++ src/gráficos/cuadrado.ts
@@ -9,21 +9,19 @@ interface IntefazDeCuadrado {
 
 export class Cuadrado extends Gráfico {
   #geometría: Geometría;
-  #render() {
+  #render = () => {
     let pts = this.#geometría.pts.obtener();
     this.contexto.moveTo(pts[0].x, pts[0].y);
     pts.forEach((pt) => {
       this.contexto.lineTo(pt.x, pt.y);
     });
     this.contexto.stroke();
-  }
+  };
 
   constructor(data: IntefazDeCuadrado) {
     super(data.canvas);
     const { tamaño, origen } = data;
     this.#geometría = new Geometría({ tamaño, origen });
-    this.render = () => {
-      this.#render();
-    };
+    this.render = this.#render;
   }
 }
